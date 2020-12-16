3f8895b3f46592ff179cd513fb2c804471b0039b
SieroSec <SieroSec@gmail.com>
2020-12-16 23:04:54 +0100

    updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// storage ctrl
+///////////////////// storage controller /////////////////////
 const StorageCtrl = (function (param) {
 
    // Public methods
@@ -7,8 +7,8 @@ const StorageCtrl = (function (param) {
    }
 })();
 
-// item ctrl
-const ItemCtrl = (function (param) {
+///////////////////// Item controller /////////////////////
+const ItemCtrl = (function () {
    // Item constructor
 
    const Item = function (id, name, calories) {
@@ -30,6 +30,31 @@ const ItemCtrl = (function (param) {
 
    // Public methods
    return {
+      getItems: function () {
+         return data.items;
+      },
+
+      addItem: function (name, calories) {
+         let ID;
+         // create item id
+         if (data.items.length > 0) {
+            ID = data.items[data.items.length - 1].id + 1;
+         } else {
+            ID = 0;
+         }
+
+         // calories to number, create new item, add to items array
+         calories = parseInt(calories);
+
+         newItem = new Item(ID, name, calories);
+         //newItem = { id, name, calories }
+         // console.log(newItem);
+
+         data.items.push(newItem);
+         console.log(data.items);
+         return newItem;
+      },
+
       logData: function () {
          return data;
       }
@@ -37,23 +62,107 @@ const ItemCtrl = (function (param) {
 
 })();
 
-// UI ctrl
+///////////////////// UI controller /////////////////////
 const UICtrl = (function (param) {
-
+   const UISelectors = {
+      itemList: '#item-list',
+      addBtn: '.add-btn',
+      itemNameInput: '#item-name',
+      itemCaloriesInput: '#item-calories'
+   }
 
    // Public methods
    return {
+      populateItemList: function (items) {
+         let html = '';
 
+         items.forEach(function (item) {
+            html += `
+            <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong><em>${item.calories}</em>
+            <a href="#" class="secondary-content"><i class="tiny material-icons edit">edit</i></a></li>`;
+
+            // insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+
+         });
+      },
+
+      addListItem: function (item) {
+         // create li element
+         const li = document.createElement('li');
+         // add class
+         li.className = 'collection-item';
+         // add id
+         li.id = `item-${item.id}`;
+
+         // add html
+         li.innerHTML = `<strong>${item.name}: </strong><em>${item.calories}</em><a href="#" class="secondary-content"><i class="tiny material-icons edit">edit</i></a>`;
+         // insert item
+         document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+      },
+      clearInput: function () {
+         document.querySelector(UISelectors.itemNameInput).value = '';
+         document.querySelector(UISelectors.itemCaloriesInput).value = '';
+      },
+      getItemInput: function () {
+         return {
+            name: document.querySelector(UISelectors.itemNameInput).value,
+            calories: document.querySelector(UISelectors.itemCaloriesInput).value
+         }
+      },
+
+      getSelectors: function () {
+         return UISelectors;
+      }
    }
 })();
 
-// App ctrl
+///////////////////// App controller /////////////////////
 const App = (function (ItemCtrl, UICtrl, StorageCtrl) {
+   // load event listeners
+   const loadEventListeners = function () {
+      // get UI selectors
+      const UISelectors = UICtrl.getSelectors();
+
+      // add item event
+      document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+   }
+
+   // add item submit
+   const itemAddSubmit = function (e) {
+      // get form input from UI controller
+      const input = UICtrl.getItemInput();
+
+      // check for name and calories input
+      if (input.name !== '' && input.calories !== '') {
+         //check if input.calories can be converte to number
+         if (!isNaN(parseInt(input.calories))) {
+            // add item to array
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+            // add item to UI
+            UICtrl.addListItem(newItem);
+            // clear fields
+            UICtrl.clearInput();
+
+         } else { console.log("Calories is not integer!"); }
+      }
+      e.preventDefault();
+   }
 
    // Public methods
    return {
       init: function () {
          console.log('Initializing App...');
+
+         // Fetch items from data structure
+         const items = ItemCtrl.getItems();
+
+         // load event listeners
+         loadEventListeners();
+
+         // populate list of items
+         UICtrl.populateItemList(items);
       }
    }
 })(ItemCtrl, UICtrl, StorageCtrl);
